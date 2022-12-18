a29384bfa20e56d009a20477db7992b05adaedd6
Dan Zenke <107815714+dnznk@users.noreply.github.com>
2022-12-18 12:18:45 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,4 @@
+const toggleMenuOpen = () => document.body.classList.toggle("open");
+
+// const navbarItem = document.querySelector(".navbar");
+// const contentContainer = document.querySelector(".container-main-content");
